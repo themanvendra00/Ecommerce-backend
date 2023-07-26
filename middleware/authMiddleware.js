77dd62cad5beb9c795f6ac0c79a51bd998bb6209
themanvendra00 <b.manvendrasingh2000@gmail.com>
2023-07-26 18:26:52 +0530

    created token authenticate middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ message: 'Authentication token missing' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid or expired token' });
+    }
+    req.userId = decoded.userId;
+    next();
+  });
+};
+
+module.exports = { authenticateToken };
