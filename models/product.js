db4ccc08f40bfbf30a3d64acad0bc2907c9f9a4e
themanvendra00 <b.manvendrasingh2000@gmail.com>
2023-07-26 18:25:32 +0530

    created schemas and their relations

---
+++ models/product.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    availability: { type: Boolean, default: true },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+module.exports = mongoose.model("Product", productSchema);
---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    cart: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: { type: Number, required: true, default: 1 },
+      },
+    ],
+    orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }],
+  },
+  {
+    versionKey: false,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
